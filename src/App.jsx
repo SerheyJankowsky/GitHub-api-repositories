f8bc48b79c089e25c0bd8510dd177a2a2f77293e
SergeyTupilow <89643267+SergeyTupilow@users.noreply.github.com>
2022-03-27 21:42:39 +0300

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import s from "./app.module.scss";
 import AppRoutes from "./Routes";
-import { useZoom } from "./hooks";
 
 function App() {
-  const zoom = useZoom(0.9);
-
   return (
     <div className={s.wrapper}>
       <AppRoutes />
